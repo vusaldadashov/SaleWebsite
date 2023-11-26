b88f9a8d0f14000ecaf2a51db38703e2cb41ffbb
Vusal <vusaldadashov99@gmail.com>
2023-11-26 21:38:41 +0100

    All Updated, Cleaned Unnecessary files

--- SaleWebsite/Program.cs
+++ SaleWebsite/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddSignalR(options =>
 
 builder.Services.AddDbContext<DataContext>(opt =>
     {
-
+        /*
         
         var DbHost = Environment.GetEnvironmentVariable("DB_HOST") ?? "localhost";
         var DbName = Environment.GetEnvironmentVariable("DB_NAME") ?? "SaleWebsiteDB";
@@ -26,8 +26,10 @@ builder.Services.AddDbContext<DataContext>(opt =>
         var ConStr = $"Server={DbHost}, 1433;Database={DbName};User ID=SA;Password={DbPassword};TrustServerCertificate=True";
         
         opt.UseSqlServer(ConStr);
-        
+        */
         //opt.UseSqlServer(builder.Configuration.GetConnectionString("SaleWebsiteDB"));
+
+        opt.UseSqlite(builder.Configuration.GetConnectionString("SaleWebsiteDB"));
     });
 
 //builder.Services.AddDistributedMemoryCache();
